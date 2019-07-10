dc016db640f8ab43f3be3be8365014911d2126f7
ElisaHery <elisa.hery@gmail.com>
2019-07-10 14:31:13 +0200

    homepage api call and add favicon

--- src/client/Views/Home/Home.js
+++ src/client/Views/Home/Home.js
@@ -2,6 +2,27 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 
 class Home extends Component {
+  state = {
+    allCountdowns: []
+  };
+
+  componentDidMount() {
+    this.callApi("http://localhost:9999/api/v1/countdown")
+      .then(res => {
+        console.log(res);
+        this.setState({ allCountdowns: res });
+      })
+      .catch(err => console.log(err));
+  }
+
+  callApi = async url => {
+    const response = await fetch(url);
+    const body = await response.json();
+    if (response.status !== 200) throw Error(body.message);
+    // console.log(body);
+    return body;
+  };
+
   render() {
     return (
       <>
