7699162248af084b74cdab94c3532442ef650a2e
Vaudem <39990188+Vaudem@users.noreply.github.com>
2019-07-11 11:56:15 +0200

    theme component

---
+++ src/client/Components/Theme/Theme.js
@@ -0,0 +1,71 @@
+
+import React, { Component } from "react";
+import { UrlAPI } from "../../utils/constants";
+
+class Theme extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+         allTheme:[],
+         theme:""
+        };
+
+        this.handleChange = this.handleChange.bind(this);
+    
+      }
+
+    handleChange(e) {
+      const { name, value } = e.target;
+      this.setState({[name] : value });
+      console.log(name, value);
+      
+      const selectTheme = document.getElementById("selectTheme");
+      const idselectedTheme = selectTheme.options[selectTheme.selectedIndex].value;
+      console.log('theme selected', idselectedTheme);
+      localStorage.setItem('idTheme', idselectedTheme);
+    };
+
+  componentDidMount() {
+    this.callApi(UrlAPI + "/theme")
+      .then(res => {
+        console.log(res[1]);
+        const results = res[1];
+        this.setState({ allTheme: results });
+      })
+      .catch(err => console.log(err));
+  }
+
+  callApi = async url => {
+    const response = await fetch(url);
+    const body = await response.json();
+    if (response.status !== 200) throw Error(body.message);
+    // console.log(body);
+    return body;
+  };
+
+  render() {
+    console.log("const url -->", UrlAPI);
+    const listCountdowns = this.state.allTheme.map((number) =>
+        <option key={number.id} value={number.id}>{number.name}</option>
+    );
+    return (
+
+        <React.Fragment>
+            <select
+                className="form-control"
+                name="theme"
+                // value={username}
+                onChange={this.handleChange} 
+                id = "selectTheme"
+            >
+            <option>Choose</option>
+            {listCountdowns}
+            </select>
+        
+        </React.Fragment>
+
+    );
+  }
+}
+
+export default Theme;
