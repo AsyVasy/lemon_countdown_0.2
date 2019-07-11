5dda017b09f9036ed87a0843080b3f7aac98cb3d
ElisaHery <elisa.hery@gmail.com>
2019-07-11 17:36:28 +0200

    add-homepage-style

--- src/client/Components/countdownItem/countdownItem.js
+++ src/client/Components/countdownItem/countdownItem.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import { UrlAPI } from "../../utils/constants";
+import Matrix from "./../../assets/matrix.jpg";
+import Cuisine from "./../../assets/cuisine.jpeg";
 
 import "./countdownItem.scss";
 
@@ -25,30 +27,48 @@ class CountdownItem extends Component {
   }
 
   render() {
+    const Background = this.props.name_theme === "matrix" ? Matrix : Cuisine;
+
     return (
       <>
-        <div className="item">{this.props.name}</div>
-        <Link
-          to={{
-            pathname: "/play-countdown",
-            state: {
-              id_countdown: this.props.id
-            }
-          }}
-        >play</Link>
-        <Link
-          to={{
-            pathname: "/edit-countdown",
-            state: {
-              id_countdown: this.props.id
-            }
-          }}
-        >
-          edit
-        </Link>
-        <button className="delete_button" onClick={e => this.handleClick(e)}>
-          Supprimer
-        </button>
+        <div className="itemWrapper">
+          <div className="picture">
+            <img className="Background" src={Background} />
+            <p className="countdown_name">{this.props.name}</p>
+          </div>
+          <div className="commands">
+            <Link
+              to={{
+                pathname: "/play-countdown",
+                state: {
+                  id_countdown: this.props.id
+                }
+              }}
+            >
+              <i className="fas fa-play fa-2x CommandItem" />
+            </Link>
+            <Link
+              to={{
+                pathname: "/edit-countdown",
+                state: {
+                  id_countdown: this.props.id
+                }
+              }}
+            >
+              <i className="fas fa-pen fa-2x CommandItem" />
+            </Link>
+            {/* <button
+              className="delete_button"
+              
+            >
+              Supprimer
+            </button> */}
+            <i
+              className="fas fa-trash-alt fa-2x CommandItem"
+              onClick={e => this.handleClick(e)}
+            />
+          </div>
+        </div>
       </>
     );
   }
