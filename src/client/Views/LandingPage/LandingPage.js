1a3feb31710dfb89ec2318d8d875d0c0aed969b3
ElisaHery <elisa.hery@gmail.com>
2019-07-12 10:19:11 +0200

    update landing page

--- src/client/Views/LandingPage/LandingPage.js
+++ src/client/Views/LandingPage/LandingPage.js
@@ -1,29 +1,25 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./landingpage.scss";
-import  logo  from "../../assets/lemoncd.png";
-
-
+import logo from "../../assets/lemoncd.png";
 
 function LandingPage() {
   return (
     <>
-    <section className="landing">
-      <figure className="logo1">
-        <img src={logo} alt="logo"/>
-      </figure>
-      
-        
-      <div className="link11">
-      <button className="btn btn-primary"><Link to="/new-countdown"> Créer un compteur</Link>{" "}</button>
-      
-      </div>
-      <div className="link12">
-      <Link to="/home"> Voir tous les compteurs</Link>{" "}
-      </div>
-    </section>
+      <section className="landing">
+        <figure className="logo1">
+          <img src={logo} alt="logo" />
+        </figure>
 
-      
+        <div className="subtitle">
+          Bienvenue sur le générateur de compte à rebours!
+        </div>
+        <div className="link11">
+          <button className="btn btn-primary">
+            <Link to="/new-countdown"> ENTRER</Link>{" "}
+          </button>
+        </div>
+      </section>
     </>
   );
 }
